533aba1e86a2ad3d15adc9e736df772d02939eaa
Judy <moujudy@gmail.com>
2012-04-10 00:31:41 -0700

    Enable user to enter name, url, and email, and store it in the database

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,24 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 import datetime
-import products.models
+from products.models import TempTrackList
 from mysite.forms import ProductForm
-
+from django.core.mail import send_mail
 
 def hello(request):
     return HttpResponse("Hello world")
-	
 
+@csrf_exempt	
 def product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            item = form.cleaned_data
-            t1 = TempTrackList.objects.create(username=item['username'],email = item['email'], product_url = item['product_url'])
-            return HttpResponseRedirect('/hello/')
-    else:
-        form = ProductForm(initial= {'username': 'ILoveShopping', 'email':'judy@yahoo.com', 'product_url':'http://haha'})
     error = False
+    if request.method == 'POST':
+       form = ProductForm(request.POST)
+       if form.is_valid():
+           item = form.cleaned_data
+           t1 = TempTrackList.objects.create(username=item['username'],email = item['email'], product_url = item['product_url'])
+           return render_to_response('current_datetime.html', {'error': error})
+    else:
+       form = ProductForm(initial= {'username': 'ILoveShopping', 'email':'judy@yahoo.com', 'product_url':'http://haha'})
     return render_to_response('welcome.html', {'form': form })
 
 def current_datetime(request):
