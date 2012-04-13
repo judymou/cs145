17634d129d8af6e7434b6513fca20a719f8edad7
Judy <moujudy@gmail.com>
2012-04-12 23:12:06 -0700

    demo.py for parsing amazon website

---
+++ mysite/mysite/webscraping_demo.py
@@ -0,0 +1,44 @@
+########################################################
+# Alain M. Lafon, 15.03.2009
+# preek.aml@gmail.com
+# 
+# HTML scraping with Python and BeautifulSoup
+# http://dispatched.ch
+########################################################
+
+
+import urllib
+import urllib2
+import string
+import sys
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+from time import time
+
+
+url = 'http://www.amazon.com/gp/product/0123745144/ref=s9_simh_gw_p14_d0_g14_i2?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=center-2&pf_rd_r=07690KXG6VKFA1C6Q93D&pf_rd_t=101&pf_rd_p=470938631&pf_rd_i=507846'
+page=urllib2.urlopen(url)
+html=page.read()
+page.close()
+
+start = time()
+targetPage1 = SoupStrainer(id = ['btAsinTitle', 'ASIN'])
+targetPage2 = SoupStrainer(attrs={'class':'priceLarge'})
+pool1=BeautifulSoup(html, targetPage1)
+pool2=BeautifulSoup(html, targetPage2)
+
+print pool1;
+print pool2;
+
+asin = pool1.findAll(id = 'ASIN', limit = 1)
+asin = asin[0]['value']
+title = pool1.findAll(id = 'btAsinTitle', limit=1)
+title = title[0].findAll(text=True)
+price = pool2.findAll(limit=1)
+price = price[0].findAll(text=True)
+
+print "title: %s" % title[0]
+print "price: %s" % price[0]
+print "asin: %s"  % asin
+elapsed = (time() - start)
+print elapsed
+
