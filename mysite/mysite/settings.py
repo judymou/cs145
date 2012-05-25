fdfbd3fd0992f9787822d7569e6fde9d394f41a4
unknown <Lucia@Lucia-HP.(none)>
2012-05-25 16:44:49 -0700

    setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,10 +22,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'onomnomdb',                      # Or path to database file if using sqlite3.
-        'USER': 'judymou',                      # Not used with sqlite3.
-        'PASSWORD': 'onomnom',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/Users/Lucia/Documents/cs145new/mysite/sqlite3db/mydb.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
