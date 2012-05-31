db1bd3d94953676253a2bf0de5b31663394ab933
judyMou <moujudy@gmail.com>
2012-05-31 01:20:55 -0700

    comment source code

---
+++ mysite/threadedcomments-0.4/threadedcomments/forms.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django import newforms as forms
+from models import DEFAULT_MAX_COMMENT_LENGTH
+from models import FreeThreadedComment, ThreadedComment
+from django.contrib.contenttypes.models import ContentType
+
+class ThreadedCommentForm(forms.ModelForm):
+    """
+    Form which can be used to validate data for a new ThreadedComment.
+    It consists of just two fields: ``comment``, and ``markup``.
+    
+    The ``comment`` field is the only one which is required.
+    """
+
+    comment = forms.CharField(
+        max_length = DEFAULT_MAX_COMMENT_LENGTH,
+        widget = forms.Textarea
+    )
+
+    class Meta:
+        model = ThreadedComment
+        fields = ('comment', 'markup')
+
+class FreeThreadedCommentForm(forms.ModelForm):
+    """
+    Form which can be used to validate data for a new FreeThreadedComment.
+    It consists of just a few fields: ``comment``, ``name``, ``website``,
+    ``email``, and ``markup``.
+    
+    The fields ``comment``, and ``name`` are the only ones which are required.
+    """
+
+    comment = forms.CharField(
+        max_length = DEFAULT_MAX_COMMENT_LENGTH,
+        widget = forms.Textarea
+    )
+
+    class Meta:
+        model = FreeThreadedComment
+        fields = ('comment', 'name', 'website', 'email', 'markup')
